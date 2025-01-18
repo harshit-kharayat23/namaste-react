4134cdb5739a7ae8ddabfd9a13b5ee5379fcc3de
Harshit Kharayat <harshitkharayat224@gmail.com>
2025-01-18 11:56:47 +0530

    ignite the app

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 const parent = React.createElement(
     "div",
     { id: "parent" },
     React.createElement(
         "div",
         { id: "child" },
-        React.createElement("h1", { id: "heading" }, "hii guys")
+        React.createElement("h1", { id: "heading" }, "hello Harshit")
     )
 );
 
-const root = ReactDOM.createRoot(document.getElementById("Root"));  // ✅ Correct
-console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("Root")); // ✅ Required in React 19
 root.render(parent);
+
+
